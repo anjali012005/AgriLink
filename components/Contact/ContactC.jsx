47215dd4a9ec5f2760693414f12d0021cc9a14a0
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-09-04 23:54:10 +0530

    Added basic components

---
+++ components/Contact/ContactC.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ContactC.css'
+
+const ContactC = () => {
+  return (
+    <div>
+      Contact
+    </div>
+  )
+}
+
+export default ContactC
---
+++ components/Home/HomeC.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './HomeC.css'
+
+const HomeC = () => {
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
+
+export default HomeC
